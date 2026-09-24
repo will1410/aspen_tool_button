b32ddd6ef8fc95461344884079c03850278d3f20
George Williams <will1410@users.noreply.github.com>
2026-09-24 15:12:42 -0500

    change reset button function

--- script.js
+++ script.js
@@ -77,7 +77,7 @@ $(document).ready(function () {
       event.preventDefault();
 
       $("input, textarea").val("");
-      $("#type_link, #name_radio-1").prop("checked", true);
+      $("#type_primary, #name_radio-1").prop("checked", true);
       $("#button_name").prop("required", true);
     });
 
