1e250d32b0905b4e1c80b35e4842b58b1b39e296
George Williams <will1410@users.noreply.github.com>
2026-09-24 14:45:21 -0500

    Update form validation

--- script.js
+++ script.js
@@ -13,24 +13,44 @@ $(document).ready(function () {
         console.log('button_name: ' + button_name);
         console.log('button_url: ' + button_url);
 
+        let buttonNameRaw = $('input[name="name_selection"]:checked').val() || '-ERROR-';
+
+      /* Makes URL a required field always */
         if (!button_url) {
           $("#button_url")[0].reportValidity();
+          return;
+        }
+
+      /* Make name required if name is user defined */
+        $('input[name="name_selection"]').change(function () {
+          if ($(this).val() === 'user_defined') {
+            $("#button_name").prop("required", true);
+          } else {
+            $("#button_name").prop("required", false);
+          }
+        });
+
+      /* Validate form to make sure name and url are not blank */
+        if (!button_url) {
+          $("#button_url")[0].reportValidity();
+          return;
+        }
+
+        if ($("#button_name").prop("required") && !button_name) {
+          $("#button_name")[0].reportValidity();
           return;
         }
 
         let buttonType = $('input[name="button_type"]:checked').val() || 'primary';
         let buttonClass = 'btn btn-' + buttonType + ' btn-lg';
 
-        console.log('buttonClass: ' + buttonClass);
-
-        let buttonNameRaw = $('input[name="name_selection"]:checked').val() || '-ERROR-';
         let buttonName = ''
 
         if (buttonNameRaw === 'user_defined') {
-          buttonName = button_name
-        } else buttonName = buttonNameRaw
-
-        console.log('buttonName: ' + buttonName);
+          buttonName = button_name;
+        } else {
+          buttonName = buttonNameRaw;
+        }
 
         $("#output").val(
           '<a class="' + 
@@ -41,11 +61,10 @@ $(document).ready(function () {
           buttonName +
           '</a>'
         );
-        
 
     });
 
-  /* Copies output to clipboard */
+  /* Adds function to "Copy . . . " button */
     $("#copy_to_clipboard").click(function () {
       event.preventDefault();
       var copy_output = $("#output").val().trim();
@@ -53,11 +72,13 @@ $(document).ready(function () {
       navigator.clipboard.writeText(copy_output);
     })
 
-  /* Adds function to reset button */
-    $("#reset").click(function () {
+  /* Adds function to "Reset" button */
+    $("#reset").click(function (event) {
       event.preventDefault();
+
       $("input, textarea").val("");
-      $("#type_primary, #name_radio-1").prop("checked", true);
+      $("#type_link, #name_radio-1").prop("checked", true);
+      $("#button_name").prop("required", true);
     });
 
 });
